be88a3715973e100a2bf23ae9b7b9327f6a08bde
Brian Schalme <bschalme@airspeed.ca>
2023-05-10 11:58:44 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Serving Web Content
+
+This is the exercise from the Spring Getting Started guide [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/).
+
---
+++ src/main/java/ca/airspeed/servingwebcontent/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package ca.airspeed.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
+
+}
