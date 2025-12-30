33d1ac3a07d8f1ee9f7a44968922a6306c56f9e3
Brian Schalme <bschalme@airspeed.ca>
2025-12-30 14:14:58 -0600

    Change to the greeting text.

--- src/test/java/ca/airspeed/servingwebcontent/GreetingControllerTest.java
+++ src/test/java/ca/airspeed/servingwebcontent/GreetingControllerTest.java
@@ -1,5 +1,14 @@
 package ca.airspeed.servingwebcontent;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.jsoup.Jsoup.parse;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -10,15 +19,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.jsoup.Jsoup.parse;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
@@ -45,7 +45,7 @@ class GreetingControllerTest {
         assertThat("Body;", body, notNullValue());
         Elements paragraphs = body.getElementsByTag("p");
         assertThat("<p> tags;", paragraphs, hasSize(greaterThan(0)));
-        assertThat("Greeting text;", paragraphs.get(0).text(), is("Hello, World!"));
+        assertThat("Greeting text;", paragraphs.get(0).text(), is("Salutations, World!"));
     }
 
     @Test
@@ -62,6 +62,6 @@ class GreetingControllerTest {
         assertThat("Body;", body, notNullValue());
         Elements paragraphs = body.getElementsByTag("p");
         assertThat("<p> tags;", paragraphs, hasSize(greaterThan(0)));
-        assertThat("Greeting text;", paragraphs.get(0).text(), is("Hello, Brian!"));
+        assertThat("Greeting text;", paragraphs.get(0).text(), is("Salutations, Brian!"));
     }
 }
